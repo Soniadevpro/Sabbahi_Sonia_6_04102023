0435f9ffbe8e29f2b7959eae816259c381bd8550
Sonia Sabbahi <soniadevpro@gmail.com>
2023-10-11 23:04:16 +0200

    fonction fetch

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/js/index.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/js/index.js
@@ -1,5 +1,19 @@
-fetch("http://localhost:5678/api/categories")
-  .then((res) => res.json())
-  .then((data) => {
-    console.log("ici");
-  });
+// fonction asynchrone pour récupérer les catégories
+async function fetchCateg() {
+  // Effectuer une requête HTTP asynchrone vers l'URL spécifiée
+  const requete = await fetch("http://localhost:5678/api/categories");
+
+  // Vérif de la requête
+  if (requete.ok === true) {
+    // Si la requête réussi, renvoyer les données JSON
+    return requete.json();
+  } else {
+    // Si la requête a échoué, lancer un message d'erreur
+    throw new Error("Impossible de contacter le serveur");
+  }
+}
+
+// Appeler la fonction
+fetchCateg().then((categories) => {
+  console.log(categories);
+});
