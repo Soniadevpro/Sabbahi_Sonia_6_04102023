2b605a32d05e8bfe465ea6861f3593489d38d150
Sonia Sabbahi <soniadevpro@gmail.com>
2023-10-11 20:52:12 +0200

    html css filter

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/js/index.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/js/index.js
@@ -1,1 +1,10 @@
-console.log("lololol");
+///FETCH-----------
+
+fetch("http://localhost:5678/api/categories")
+  .then((res) => res.json())
+  .then((lien) => console.log(lien));
+
+/////////////////////////////////////////////
+fetch("http://localhost:5678/api/works")
+  .then((res) => res.json())
+  .then((lien) => console.log(lien));
