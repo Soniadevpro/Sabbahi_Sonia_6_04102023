87a96199eda2a525a62a33319ab81ae749dee537
Sonia Sabbahi <soniadevpro@gmail.com>
2023-10-12 11:28:08 +0200

    ajout des works

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/js/index.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/js/index.js
@@ -16,12 +16,31 @@ async function init() {
 
 init();
 
-const gallery = document.querySelector(".filterbar");
+const gallery = document.querySelector(".gallery");
 
-function showCategories(categories) {
-  // Creation des categories HTML
-}
+function showCategories(categories) {}
+// Creation des categories HTML
 
 function showWorks(works) {
+  for (let i = 0; i < works.length; i++) {
+    //créa article et apport du contenu dynamique
+    const worksProjets = document.createElement("figure");
+    //atribution des class
+    worksProjets.setAttribute("class", "figure");
+    //attribution d'un id
+    worksProjets.setAttribute("id", works[i].id);
+    // attribution des categories
+    worksProjets.setAttribute("data-category", works[i].categoryId);
+    //ajout d'une image
+    const img = document.createElement("img");
+    img.src = works[i].imageUrl;
+    //ajout d'un sous titre
+    const sousTitre = document.createElement("figsub");
+    sousTitre.innerHTML = works[i].title;
+    //asso enfant parent
+    gallery.appendChild(worksProjets);
+    worksProjets.appendChild(img);
+    worksProjets.appendChild(sousTitre);
+  }
   // Creation des works HTML
 }
