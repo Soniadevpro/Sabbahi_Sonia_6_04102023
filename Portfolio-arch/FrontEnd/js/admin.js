6fc3cac26125d259f21525fde081701ea87ff577
Sonia Sabbahi <soniadevpro@gmail.com>
2023-10-25 19:51:50 +0200

    maj token

--- Portfolio-arch/FrontEnd/js/admin.js
+++ Portfolio-arch/FrontEnd/js/admin.js
@@ -1,1 +1,15 @@
 // MODELE POUR STYLE LORSQUE CONNECTE
+
+let token = localStorage.getItem("authToken");
+const changeLog = document.querySelector(".login");
+const editBanner = document.querySelector(".edit-banner");
+const byeFilterBar = document.querySelector(".filterbar");
+if (token) {
+  editBanner.style.display = "flex";
+  byeFilterBar.style.display = "none";
+  changeLog.innerHTML = "logout";
+  changeLog.addEventListener("click", () => {
+    localStorage.removeItem("authToken");
+    window.location.href = "./login.html";
+  });
+}
